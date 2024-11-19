cd2c3e3d4c60c61777295d4a0fea3cfe0e6132b5
Javier Manotas Ruiz <151833192+javimanotas@users.noreply.github.com>
2024-11-19 13:17:26 +0100

    feat: add vector operations

--- Utils.hs
+++ Utils.hs
@@ -7,16 +7,12 @@ import Control.Monad.State
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 
-
 {-------------------- Memoization --------------------}
-
 
 type Memo k v = State (Map.Map k v) v
 
-
 evalMemo :: Memo k v -> v
 evalMemo x = evalState x Map.empty
-
 
 memoize :: (Ord k) => k -> Memo k v -> Memo k v
 memoize key compute = do
@@ -29,28 +25,22 @@ memoize key compute = do
             put (Map.insert key value memo)
             return value
 
-
 {-------------------- Parsing --------------------}
-
 
 type StringParser = Parsec String ()
 
-
 fileLines :: FilePath -> IO [String]
 fileLines path = lines <$> readFile path
-
 
 parseLines :: [String] -> StringParser a -> IO [a]
 parseLines fLines parser = case mapM (parse parser "") fLines of
     Left err -> print err >> error "parsing error"
     Right ok -> return ok
 
-
 parseFile :: FilePath -> StringParser a -> IO [a] 
 parseFile path parser = do
     fLines <- fileLines path
     parseLines fLines parser
-
 
 integral :: (Read a, Integral a) => StringParser a
 integral = negative <|> positive
@@ -58,33 +48,25 @@ integral = negative <|> positive
         negative = char '-' >> (negate . read <$> many1 digit)
         positive = optional (char '+') >> (read <$> many1 digit)
 
-
 {-------------------- Lists --------------------}
-
 
 splitWhen :: (a -> Bool) -> [a] -> [[a]]
 splitWhen f = filter (not . any f) . groupBy ((==) `on` f)
 
-
 howMany :: (a -> Bool) -> [a] -> Int
 howMany f = length . filter f
-
 
 chunksOf :: Int -> [a] -> [[a]]
 chunksOf _ [] = []
 chunksOf n l = take n l : chunksOf n (drop n l)
 
-
 mapi :: (Int -> a -> b) -> [a] -> [b]
 mapi f = zipWith f [0..]
-
 
 filteri :: (Int -> a -> Bool) -> [a] -> [a]
 filteri f = map snd . filter (uncurry f) . zip [0..]
 
-
 {-------------------- Sets --------------------}
-
 
 intersections :: (Foldable f, Ord a) => f (Set.Set a) -> Set.Set a
 intersections sets
@@ -93,12 +75,17 @@ intersections sets
 
 {-------------------- Data structures --------------------}
 
-
 type Graph k = Map.Map k [k]
-
 
 type Matrix = Map.Map (Int, Int)
 
-
 matrixFromList :: [[a]] -> Matrix a
 matrixFromList = Map.fromList . concat . zipWith (\n -> zip (map (n, ) [0..])) [0..]
+
+{-------------------- Vectors --------------------}
+
+(/+) :: Num a => (a, a) -> (a, a) -> (a, a)
+(a, b) /+ (c, d) = (a + c, b + d)
+
+(/*) :: Num a => a -> (a, a) -> (a, a)
+k /* (a, b) = (k * a, k * b)
